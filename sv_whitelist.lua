0fc3efbc8a5111fb333045b3a010feb3b15d60dd
Rodion <54954576+noaccessl@users.noreply.github.com>
2020-07-09 16:59:02 +0300

    Update sv_whitelist.lua
    removed a "shit"

--- sv_whitelist.lua
+++ sv_whitelist.lua
@@ -1,7 +1,3 @@
-local hook_Add = hook.Add 
-local hook_Call = hook.Call 
-local util_SteamIDFrom64 = util.SteamIDFrom64 
-
 WhiteList_Enabled = true 
 
 WhiteList = { 
@@ -9,15 +5,8 @@ WhiteList = {
 } 
 
 hook_Add( 'CheckPassword', 'CheckWhiteList', function( SteamID64, a, b, c, SteamName ) 
-    if ( not WhiteList_Enabled ) then 
-        local Bool, Reason = hook_Call( 'CheckPassword', SteamID64, a, b, c, SteamName ) 
-        if ( Bool ~= true ) then 
-            return Bool, Reason 
-        else 
-            return true 
-        end 
-    else 
-	    local SteamID = util_SteamIDFrom64( SteamID64 ) 
+    if ( WhiteList_Enabled ) then 
+	    local SteamID = util.SteamIDFrom64( SteamID64 ) 
 
 	    if ( not WhiteList[ SteamID ] ) then 
 	    	print( SteamName .. ' ( ' .. SteamID .. ' ) not in whitelist, kicking...' ) 
