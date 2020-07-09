2414a969a926c1524114a42cc306ead4ff05fae8
Rodion <54954576+noaccessl@users.noreply.github.com>
2020-07-09 17:05:16 +0300

    Update sv_whitelist.lua
    replaced steamid into steamid64

--- sv_whitelist.lua
+++ sv_whitelist.lua
@@ -1,15 +1,13 @@
 WhiteList_Enabled = true 
 
 WhiteList = { 
-	-- Example: [ 'steamid here' ] = true, 
+	-- Example: [ 'SteamID64 Here' ] = true, 
 } 
 
 hook.Add( 'CheckPassword', 'CheckWhiteList', function( SteamID64, a, b, c, SteamName ) 
     if ( WhiteList_Enabled ) then 
-	    local SteamID = util.SteamIDFrom64( SteamID64 ) 
-
-	    if ( not WhiteList[ SteamID ] ) then 
-	    	print( SteamName .. ' ( ' .. SteamID .. ' ) not in whitelist, kicking...' ) 
+	    if ( not WhiteList[ SteamID64 ] ) then 
+	    	print( SteamName .. ' ( ' .. SteamID64 .. ' ) not in whitelist, kicking...' ) 
 	    	return false, 'Ты не в белом списке, или же на сервере технические работы\nYou are not in the white list, or server on the technical works' 
 	    end 
     end 
