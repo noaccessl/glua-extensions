1215d4a6493c94848ccb4c827b91cd2785648e0b
Rodion <54954576+noaccessl@users.noreply.github.com>
2020-07-18 19:57:33 +0300

    Update string2.lua
    fix

--- lua/includes/extensions/string2.lua
+++ lua/includes/extensions/string2.lua
@@ -1,13 +1,16 @@
 
 local string = string 
 
+local string_upper = string.upper 
+local string_lower = string.lower 
+
 local lowers = {['А']='а',['Б']='б',['В']='в',['Г']='г',['Д']='д',['Е']='е',['Ё']='ё',['Ж']='ж',['З']='з',['И']='и',['Й']='й',['К']='к',['Л']='л',['М']='м',['Н']='н',['О']='о',['П']='п',['Р']='р',['С']='с',['Т']='т',['У']='у',['Ф']='ф',['Х']='х',['Ц']='ц',['Ч']='ч',['Ш']='ш',['Щ']='щ',['Ъ']='ъ',['Ы']='ы',['Ь']='ь',['Э']='э',['Ю']='ю',['Я']='я'} 
 local uppers = {['а']='А',['б']='Б',['в']='В',['г']='Г',['д']='Д',['е']='Е',['ё']='Ё',['ж']='Ж',['з']='З',['и']='И',['й']='Й',['к']='К',['л']='Л',['м']='М',['н']='Н',['о']='О',['п']='П',['р']='Р',['с']='С',['т']='Т',['у']='У',['ф']='Ф',['х']='Х',['ц']='Ц',['ч']='Ч',['ш']='Ш',['щ']='Щ',['ъ']='Ъ',['ы']='Ы',['ь']='Ь',['э']='Э',['ю']='Ю',['я']='Я'} 
 
 function string.upper( str ) 
     local result = '' 
     for char in string.gmatch( str, '[%z\x01-\x7F\xC2-\xF4][\x80-\xBF]*' ) do 
-    	result = result .. ( uppers[ char ] or string.upper( char ) ) 
+    	result = result .. ( uppers[ char ] or string_upper( char ) ) 
     end
 
     return result 
@@ -16,7 +19,7 @@ end
 function string.lower( str ) 
     local result = '' 
     for char in string.gmatch( str, '[%z\x01-\x7F\xC2-\xF4][\x80-\xBF]*' ) do 
-    	result = result .. ( lowers[ char ] or string.lower( char ) ) 
+    	result = result .. ( lowers[ char ] or string_lower( char ) ) 
     end
 
     return result 
