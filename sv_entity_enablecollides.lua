9fb2605b6958a332f203158c414dfe07e4f2a3cb
Rodion <54954576+noaccessl@users.noreply.github.com>
2020-07-18 15:37:19 +0300

    Create sv_entity_enablecollides.lua

---
+++ sv_entity_enablecollides.lua
@@ -0,0 +1,31 @@
+local Entity = FindMetaTable( 'Entity' ) 
+
+-- Enables/Disables collisions of a certain object forcibly.
+function Entity:EnableCollides( bool ) 
+    local Phys = self:GetPhysicsObject() 
+    local PhysCount = self:GetPhysicsObjectCount() 
+
+    if ( not IsValid( Phys ) ) then return end 
+
+    if ( PhysCount == 1 ) then 
+        Phys:EnableCollisions( bool ) 
+    end 
+
+    if PhysCount > 1 then 
+        for Num = 0, PhysCount - 1 do 
+            CurPhys = self:GetPhysicsObjectNum( Num ) 
+            if ( IsValid( CurPhys ) ) then 
+                if ( bool == true ) then 
+                    CurPhys:ClearGameFlag( FVPHYSICS_NO_SELF_COLLISIONS ) 
+                else 
+                    CurPhys:AddGameFlag( FVPHYSICS_NO_SELF_COLLISIONS ) 
+                end 
+
+                CurPhys:EnableCollisions( bool ) 
+            end 
+        end 
+    end 
+
+    self:SetCollisionGroup( Either( bool == true, COLLISION_GROUP_NONE, COLLISION_GROUP_WORLD ) ) 
+    self:DrawShadow( true ) 
+end 
