cd2e6b4082a5215559530bb56b346335137b1779
Rodion <54954576+noaccessl@users.noreply.github.com>
2020-08-12 13:59:10 +0300

    Update sv_entity_enablecollides.lua
    removed some shit, again...

--- sv_entity_enablecollides.lua
+++ sv_entity_enablecollides.lua
@@ -5,17 +5,17 @@ function Entity:EnableCollides( bool )
     local Phys = self:GetPhysicsObject()
     local PhysCount = self:GetPhysicsObjectCount()
 
-    if ( not IsValid( Phys ) ) then return end
+    if not IsValid( Phys ) then return end
 
-    if ( PhysCount == 1 ) then
+    if PhysCount == 1 then
         Phys:EnableCollisions( bool )
     end
 
     if PhysCount > 1 then
         for Num = 0, PhysCount - 1 do
             CurPhys = self:GetPhysicsObjectNum( Num )
-            if ( IsValid( CurPhys ) ) then
-                if ( bool ) then
+            if IsValid( CurPhys ) then
+                if bool then
                     CurPhys:ClearGameFlag( FVPHYSICS_NO_SELF_COLLISIONS )
                 else
                     CurPhys:AddGameFlag( FVPHYSICS_NO_SELF_COLLISIONS )
