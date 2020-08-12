eeeb5d08a7e5bb37478848de4396fdbc022c5f00
Rodion <54954576+noaccessl@users.noreply.github.com>
2020-08-12 13:24:29 +0300

    Update sv_whitelist.lua
    Rewrote the code a bit

--- sv_whitelist.lua
+++ sv_whitelist.lua
@@ -1,14 +1,16 @@
-WhiteList_Enabled = true 
+WhiteListEnabled = true
 
-WhiteList = { 
-	-- Example: [ 'SteamID64 Here' ] = true, 
-} 
+WhiteList = {
+	-- Example: [ 'SteamID64 Here' ] = true,
+}
 
-hook.Add( 'CheckPassword', 'CheckWhiteList', function( SteamID64, a, b, c, SteamName ) 
-    if ( WhiteList_Enabled ) then 
-	    if ( not WhiteList[ SteamID64 ] ) then 
-	    	print( SteamName .. ' ( ' .. SteamID64 .. ' ) not in whitelist, kicking...' ) 
-	    	return false, 'Ты не в белом списке, или же на сервере технические работы\nYou are not in the white list, or server on the technical works' 
-	    end 
-    end 
-end ) 
+local message = '' -- For example, you are not in the whitelist
+
+hook.Add( 'CheckPassword', 'CheckWhiteList', function( SteamID64, a, b, c, SteamName )
+    if ( WhiteListEnabled ) then
+	    if ( not WhiteList[ SteamID64 ] ) then
+	    	print( SteamName .. ' ( ' .. SteamID64 .. ' ) not in whitelist, kicking...' )
+	    	return false, message
+	    end
+    end
+end )
