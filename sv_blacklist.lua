3023272b095ba81cd962baea1f6c557a078da08f
Rodion <54954576+noaccessl@users.noreply.github.com>
2020-08-12 14:01:36 +0300

    Update sv_blacklist.lua
    removed some shit

--- sv_blacklist.lua
+++ sv_blacklist.lua
@@ -1,14 +1,16 @@
-BlackList_Enabled = true 
+BlackListEnabled = true
 
-BlackList = { 
-	-- Example: [ 'SteamID64 Here' ] = true, 
-} 
+BlackList = {
+	-- Example: [ 'SteamID64 Here' ] = true,
+}
 
-hook.Add( 'CheckPassword', 'CheckBlackList', function( SteamID64, a, b, c, SteamName ) 
-    if ( BlackList_Enabled ) then 
-	    if ( BlackList[ SteamID64 ] ) then 
-	    	print( SteamName .. ' ( ' .. SteamID64 .. ' ) is in blacklist, kicking...' ) 
-	    	return false, 'Ты в чёрном списке\nYou are in the blacklist' 
-	    end 
-    end 
-end ) 
+local message = '' -- For example, you are in the Blacklist
+
+hook.Add( 'CheckPassword', 'CheckBlackList', function( SteamID64, a, b, c, SteamName )
+    if ( BlackListEnabled ) then
+	    if ( BlackList[ SteamID64 ] ) then
+	    	print( SteamName .. ' ( ' .. SteamID64 .. ' ) is in blacklist, kicking...' )
+	    	return false, message
+	    end
+    end
+end )
