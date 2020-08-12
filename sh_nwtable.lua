30bb2ca1150c318be4debfed2052d17f5dd997dc
Rodion <54954576+noaccessl@users.noreply.github.com>
2020-08-12 13:21:10 +0300

    Update sh_nwtable.lua
    added several similar features

--- sh_nwtable.lua
+++ sh_nwtable.lua
@@ -1,9 +1,30 @@
-local Entity = FindMetaTable( 'Entity' ) 
+local Entity = FindMetaTable 'Entity'
 
-function Entity:SetNWTable( Key, Table ) 
-	self:SetNWString( Key, util.TableToJSON( Table ) ) 
-end 
+local encode = util.TableToJSON -- Can be spon.encode, pon2.encode or some other encoding method ( table to string )
+local decode = util.JSONToTable -- Can be spon.decode, pon2.decode or some other decoding method ( string to table )
 
-function Entity:GetNWTable( Key ) 
-	return util.JSONToTable( self:GetNWString( Key ) ) or {} 
-end 
+function Entity:SetNWTable( key, tbl )
+	self:SetNWString( key, encode( tbl ) )
+end
+
+function Entity:GetNWTable( key )
+	return decode( self:GetNWString( key ) ) or {}
+end
+
+
+function Entity:SetNW2Table( key, tbl )
+	self:SetNW2String( key, encode( tbl ) )
+end
+
+function Entity:GetNW2Table( key )
+	return decode( self:GetNW2String( key ) ) or {}
+end
+
+
+function SetGlobalTable( index, tbl )
+	SetGlobalString( index, encode( tbl ) )
+end
+
+function GetGlobalTable( index )
+	return decode( GetGlobalString( index ) ) or {}
+end
