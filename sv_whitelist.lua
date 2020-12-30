b3fc6ea0e5121c1574a306cc8bc50a5a2a1a0a26
Rodion <54954576+noaccessl@users.noreply.github.com>
2020-12-30 16:22:11 +0300

    Update sv_whitelist.lua

--- sv_whitelist.lua
+++ sv_whitelist.lua
@@ -1,15 +1,17 @@
 WhiteListEnabled = true
 
 WhiteList = {
-	-- Example: [ 'SteamID64 Here' ] = true,
+	-- Example: [ 'SteamID or 64 Here' ] = true,
 }
 
-local message = '' -- For example, you are not in the whitelist
+local message = '' -- For example, "You aren't in the whitelist"
 
-hook.Add( 'CheckPassword', 'CheckWhiteList', function( SteamID64, a, b, c, SteamName )
-    if ( WhiteListEnabled ) then
-	    if ( not WhiteList[ SteamID64 ] ) then
-	    	print( SteamName .. ' ( ' .. SteamID64 .. ' ) not in whitelist, kicking...' )
+hook.Add( 'CheckPassword', 'CheckWhiteList', function( sid64, a, b, c, name )
+    if WhiteListEnabled then
+        local sid = util.SteamIDFrom64( sid64 )
+
+	    if not WhiteList[ sid64 ] or not WhiteList[ sid ] then
+	    	print( name .. ' ( ' .. sid64 .. ' ) not in whitelist, kicking...' )
 	    	return false, message
 	    end
     end
