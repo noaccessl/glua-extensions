3c48f19261df909de91e27b99ee420845b6ae3a5
Rodion <54954576+noaccessl@users.noreply.github.com>
2020-12-30 16:23:47 +0300

    Update sv_blacklist.lua

--- sv_blacklist.lua
+++ sv_blacklist.lua
@@ -1,15 +1,17 @@
 BlackListEnabled = true
 
 BlackList = {
-	-- Example: [ 'SteamID64 Here' ] = true,
+	-- Example: [ 'SteamID or 64 Here' ] = true,
 }
 
-local message = '' -- For example, you are in the Blacklist
+local message = 'You are in the Blacklist'
 
-hook.Add( 'CheckPassword', 'CheckBlackList', function( SteamID64, a, b, c, SteamName )
-    if ( BlackListEnabled ) then
-	    if ( BlackList[ SteamID64 ] ) then
-	    	print( SteamName .. ' ( ' .. SteamID64 .. ' ) is in blacklist, kicking...' )
+hook.Add( 'CheckPassword', 'CheckBlackList', function( sid64, a, b, c, name )
+    if BlackListEnabled then
+        local sid = util.SteamIDFrom64( sid64 )
+
+	    if BlackList[ sid64 ] or BlackList[ sid ] then
+	    	print( name .. ' ( ' .. sid64 .. ' ) is in blacklist, kicking...' )
 	    	return false, message
 	    end
     end
