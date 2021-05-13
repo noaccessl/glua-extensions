a0b48a8b2b9e8ef3b3edce7c82b090c3fa807759
noaccessl <rodion.noaccessl@gmail.com>
2021-05-13 18:12:42 +0300

    minor edits

--- sh_downloadwebasset.lua
+++ sh_downloadwebasset.lua
@@ -1,59 +1,82 @@
+local GetExtension = string.GetExtensionFromFilename
+
 local allowedExtensions = {
     [ 'png' ] = true,
     [ 'jpg' ] = true,
     [ 'jpeg' ] = true
 }
 
-local extension = string.GetExtensionFromFilename
+if not file.Exists( 'servers-assets', 'DATA' ) then file.CreateDir 'servers-assets' end
 
 function DownloadWebAsset( url, callback )
-    if not file.Exists( 'assets', 'DATA' ) then file.CreateDir 'assets' end
-    if not file.Exists( 'assets/cache', 'DATA' ) then file.CreateDir 'assets/cache' end
-    if not file.Exists( 'assets/cache/materials', 'DATA' ) then file.CreateDir 'assets/cache/materials' end
 
-    if not allowedExtensions[ extension( url ) ] then
-        print( 'Failed to download web asset from ' .. url .. ' for reason: the url does not point to the allowed file extension' )
-        return
-    end
+	local urlExtension = GetExtension( url )
+    if not allowedExtensions[ urlExtension ] then return print( 'Downloading web asset from ' .. url .. '...\n\t[FAIL] URL does not point to the allowed file extension' ) end
 	
 	local mat
 
-    HTTP{
+	local parameters = {
         url = url,
         method = 'GET',
         success = function( num, body, tbl )
-            if not body or body == '' then return end
+
+            if body == '' then return end
 			
 			local assetName = string.StripExtension( string.GetFileFromFilename( string.gsub( url, '.+%/', '' ) ) )
-			local fileFormat = string.format( 'assets/cache/materials/%s.%s', assetName, extension( url ) )
+			local fileFormat = 'servers-assets/' .. assetName .. '.' .. urlExtension
 
             file.Write( fileFormat, body )
 
             mat = Material( '../data/' .. fileFormat )
 
             if callback then
+
                 callback( mat )
+
             end
+
         end,
         failed = function( reason )
-            print( 'Failed to download web asset from ' .. url .. ' for reason: ' .. reason )
+
+            print( 'Downloading web asset from ' .. url .. '...\n\t[FAIL] ' .. reason )
+
         end
     }
+    HTTP( parameters )
 
     return mat
+
 end
 
--- Можно так | You can do so
-DownloadWebAsset( 'http://37.46.132.107/assets/img/banana.png', function( asset )
-	if CLIENT then
-		hook.Add( 'HUDPaint', 'DrawWebAssetExample', function()
-			surface.SetDrawColor( 255, 255, 255 )
-			surface.SetMaterial( asset )
-			surface.DrawTexturedRect( ScrW() * 0.45, ScrH() * 0.45, mat:Width(), mat:Height() )
-		end )
-	end
-end )
+--[[---------------------------------------------------------------------------
+Example #1
+---------------------------------------------------------------------------]]
+local callback = function( asset )
 
--- Or so
-local asset = DownloadWebAsset 'http://37.46.132.107/assets/img/banana.png'
-print( asset )
+	if not CLIENT then return end
+
+	local DrawWebAssetExample = function()
+
+		surface.SetDrawColor( 255, 255, 255 )
+		surface.SetMaterial( asset )
+		surface.DrawTexturedRect( ScrW() * 0.45, ScrH() * 0.45, asset:Width(), asset:Height() )
+
+	end
+	hook.Add( 'HUDPaint', 'DrawWebAssetExample', DrawWebAssetExample )
+
+end
+DownloadWebAsset( 'https://noaccessl.github.io/img/banana.png', callback )
+
+--[[---------------------------------------------------------------------------
+Example #2
+---------------------------------------------------------------------------]]
+local asset = DownloadWebAsset 'https://noaccessl.github.io/img/banana.png'
+
+local DrawWebAssetExample = function()
+
+	surface.SetDrawColor( 255, 255, 255 )
+	surface.SetMaterial( asset )
+	surface.DrawTexturedRect( ScrW() * 0.45, ScrH() * 0.45, asset:Width(), asset:Height() )
+
+end
+hook.Add( 'HUDPaint', 'DrawWebAssetExample', DrawWebAssetExample )
--- sh_hextocolor.lua
+++ sh_hextocolor.lua
@@ -1,18 +1,18 @@
 function HexToColor( hex, alpha )
-    return Color(
-        tonumber( '0x' .. hex:sub( 1, 2 ) ),
-        tonumber( '0x' .. hex:sub( 3, 4 ) ),
-        tonumber( '0x' .. hex:sub( 5, 6 ) ),
-        alpha or 255 or tonumber( '0x' .. hex:sub( 7, 8 ) )
-    )
+	return Color(
+		tonumber( '0x' .. hex:sub( 1, 2 ) ),
+		tonumber( '0x' .. hex:sub( 3, 4 ) ),
+		tonumber( '0x' .. hex:sub( 5, 6 ) ),
+		alpha or 255 or tonumber( '0x' .. hex:sub( 7, 8 ) )
+	)
 end
 
 function ColorToHex( color )
-    color = color:ToTable()
+	color = color:ToTable()
 	local result = ''
 
 	for i = 1, #color do
-	    local value = color[ i ]
+		local value = color[ i ]
 		local hex = ''
 
 		while value > 0 do
@@ -32,14 +32,10 @@ function ColorToHex( color )
 
 	return result
 end
+FindMetaTable( 'Color' ).ToHex = ColorToHex
 
 --[[---------------------------------------------------------------------------
-Example ( You can delete him )
+Example
 ---------------------------------------------------------------------------]]
 local hex = ColorToHex( Color( 200, 0, 0 ) )
-
 print( hex ) -- Output: C80000 or C80000FF (last 2 characters are transparency(alpha) )
-
-for k, v in pairs( HexToColor( hex ) ) do
-    print( k, v )
-end -- or PrintTable( HexToColor( hex ) )
