787df49eb2659d2b79d0df573d38953b15cc2cb2
noaccessl <54954576+noaccessl@users.noreply.github.com>
2021-08-25 00:10:33 +0300

    Minor edits

--- sv_doorfix.lua
+++ sv_doorfix.lua
@@ -1,14 +1,21 @@
+-- This script sets a delay on the opening/closing of the door, which helps to get rid of annoying players who do not allow the door to open.
+
 hook.Add( 'PlayerUse', 'DoorFix', function( ply, ent )
+
 	if not ply:Alive() then return false end
 
-	local class = ent:GetClass()
-	if class == 'prop_door_rotating' then
-		if CurTime() < ( ent.m_AntiDoorSpam or 0 ) then -- Prop doors can be glitched shut by mashing the use button. This script - prevents this
+	if ent:GetClass() == 'prop_door_rotating' then
+
+		if CurTime() < ( ent.m_AntiDoorSpam or 0 ) then
+
 			return false
+
 		end
 
-		ent.m_AntiDoorSpam = CurTime() + 0.85 -- You can change this value. Default is 0.85
+		ent.m_AntiDoorSpam = CurTime() + 0.85 -- Change this value if u want. Optimal is 0.85
+
 	end
 
 	return true
+
 end )
