af810a78edc2888383eda96400b16846d6a11df3
noaccessl <54954576+noaccessl@users.noreply.github.com>
2022-02-05 22:05:49 +0300

    minor edits

--- sv_entity_enablecollides.lua
+++ sv_entity_enablecollides.lua
@@ -1,31 +1,37 @@
-local Entity = FindMetaTable 'Entity'
+local Meta = debug.getregistry().Entity
 
--- Enables/Disables collisions of a certain object forcibly.
-function Entity:EnableCollides( bool )
-    local Phys = self:GetPhysicsObject()
-    local PhysCount = self:GetPhysicsObjectCount()
+function Meta:EnableCollides( bEnable )
 
-    if not IsValid( Phys ) then return end
+	local Phys = self:GetPhysicsObject()
+	local PhysCount = self:GetPhysicsObjectCount()
 
-    if PhysCount == 1 then
-        Phys:EnableCollisions( bool )
-    end
+	if IsValid( Phys ) == false then return end
 
-    if PhysCount > 1 then
-        for Num = 0, PhysCount - 1 do
-            CurPhys = self:GetPhysicsObjectNum( Num )
-            if IsValid( CurPhys ) then
-                if bool then
-                    CurPhys:ClearGameFlag( FVPHYSICS_NO_SELF_COLLISIONS )
-                else
-                    CurPhys:AddGameFlag( FVPHYSICS_NO_SELF_COLLISIONS )
-                end
+	if PhysCount == 1 then
+		Phys:EnableCollisions( bEnable )
+	elseif PhysCount > 1 then
 
-                CurPhys:EnableCollisions( bool )
-            end
-        end
-    end
+		for i = 0, PhysCount - 1 do
 
-    self:SetCollisionGroup( bool and COLLISION_GROUP_NONE or COLLISION_GROUP_WORLD )
-    self:DrawShadow( true )
+			CurPhys = self:GetPhysicsObjectNum( i )
+
+			if IsValid( CurPhys ) then
+
+				if bEnable then
+					CurPhys:ClearGameFlag( FVPHYSICS_NO_SELF_COLLISIONS )
+				else
+					CurPhys:AddGameFlag( FVPHYSICS_NO_SELF_COLLISIONS )
+				end
+
+				CurPhys:EnableCollisions( bool )
+
+			end
+
+		end
+
+	end
+
+	self:SetCollisionGroup( bEnable and COLLISION_GROUP_NONE or COLLISION_GROUP_WORLD )
+	self:DrawShadow( true )
+
 end
