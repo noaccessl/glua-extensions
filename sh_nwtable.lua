33387342fb531aaaaae8a47a7db46a868ae06e9a
noaccessl <54954576+noaccessl@users.noreply.github.com>
2022-02-05 21:51:31 +0300

    Update sh_nwtable.lua

--- sh_nwtable.lua
+++ sh_nwtable.lua
@@ -1,22 +1,22 @@
-local Entity = FindMetaTable 'Entity'
+local Meta = debug.getregistry().Entity
 
 local encode = util.TableToJSON -- Can be spon.encode, pon2.encode or some other encoding method ( table to string )
 local decode = util.JSONToTable -- Can be spon.decode, pon2.decode or some other decoding method ( string to table )
 
-function Entity:SetNWTable( key, tbl )
+function Meta:SetNWTable( key, tbl )
 	self:SetNWString( key, encode( tbl ) )
 end
 
-function Entity:GetNWTable( key )
+function Meta:GetNWTable( key )
 	return decode( self:GetNWString( key ) ) or {}
 end
 
 
-function Entity:SetNW2Table( key, tbl )
+function Meta:SetNW2Table( key, tbl )
 	self:SetNW2String( key, encode( tbl ) )
 end
 
-function Entity:GetNW2Table( key )
+function Meta:GetNW2Table( key )
 	return decode( self:GetNW2String( key ) ) or {}
 end
 
